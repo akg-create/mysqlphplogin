36b23d5364d7782830ea6bcf7acca537d1810910
akg-create <abbie.grooms@live.com>
2025-06-30 14:41:59 -0400

    Create user_table.sql

---
+++ user_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(
+  id INT AUTO_INCREMENT PRIMARY KEY, 
+  username VARCHAR(50) NOT NULL UNIQUE, 
+  password VARCHAR(255) NOT NULL
+);
+
+-- Insert test user 
+INSERT INTO users(username, password)
+VALUES ('admin', MD5('admin123')
