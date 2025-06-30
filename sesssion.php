c94540a3dcc17d5ef6cbf1d1eb9edb09181f67da
akg-create <abbie.grooms@live.com>
2025-06-30 15:41:18 -0400

    Update sesssion.php

--- sesssion.php
+++ sesssion.php
@@ -6,14 +6,14 @@ $timeout = 60; // 1 minute
 if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $timeout)) {
     session_unset();
     session_destroy();
-    header("Location: login.php");
+    header("Location: index.php");
     exit();
 }
 
 $_SESSION['last_activity'] = time();
 
 if (!isset($_SESSION['username'])) {
-    header("Location: login.php");
+    header("Location: index.php");
     exit();
 }
 ?>
