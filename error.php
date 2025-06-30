7e592810e088aba96b72dc849ed7a0dbfa52fe63
akg-create <abbie.grooms@live.com>
2025-06-30 15:05:45 -0400

    Create error.php

---
+++ error.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html> 
+<html>
+  <head><title>Login Error</title>
+  </head>
+  <body>
+    <h2>Login Failed!</h2>
+    <p>Invalid username or password.</p>
+    <a href="login.php">Try Again</a>
+  </body>
+</html>
