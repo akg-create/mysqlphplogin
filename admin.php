932336e9aba5396a67de5d01c367803fdb06c164
akg-create <abbie.grooms@live.com>
2025-07-01 14:45:01 -0400

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,6 +1,13 @@
 <?php 
 include("session.php"); ?>
 
+<?php
+// Disable browser/server caching
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Expires: Sat, 1 Jan 2000 00:00:00 GMT");
+header("Pragma: no-cache");
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
