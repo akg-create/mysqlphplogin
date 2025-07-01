e5af20e78505a2cc578db6a2f670a9e250ae8410
akg-create <abbie.grooms@live.com>
2025-07-01 14:34:26 -0400

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,10 +3,15 @@ include("session.php"); ?>
 
 <!DOCTYPE html>
 <html>
-  <head><title>Admin Page</title></head>
+  <head>
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
   <body>
-    <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
-    <p>You are in the protected Admin area.</p>
-    <a href="logout.php">Logout</a>
+    <div class="container">
+      <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
+      <p>You are in the protected Admin area.</p>
+      <a href="logout.php">Logout</a>
+    </div>
   </body>
 </html>
